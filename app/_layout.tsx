ea7920652e9bb740c5c920b6b244bf0a9e09d3ac
Dhriti <134310082+parikhdp@users.noreply.github.com>
2025-04-13 19:40:33 +0530

    Added basic changes to layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,22 @@
 import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <>
+    <StatusBar style="light" />
+    <Stack
+      screenOptions={{
+        headerShown: false,
+        contentStyle: { backgroundColor: "#fff" },
+        animation: "slide_from_right",
+        header: () => null,
+        navigationBarHidden: true, //hides bottom navigation bar in android
+      }}
+      >
+    <Stack.Screen name="index" options={{ headerShown: false,}}/>
+    <Stack.Screen name="(auth)" />
+    </Stack>
+    </>
+  )
 }
